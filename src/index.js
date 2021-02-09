d39c92630730af78f2c84aad91ee433280c54d62
Filipanunes07 <filipanunes@FilipaNunes.localdomain>
2021-02-09 09:06:36 +0000

    Express4

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ const app = express();
 const connection = require("./config");
 
 
+
 connection.connect(function (err) {
   if (err) {
     console.error("error connecting: " + err.stack);
@@ -13,12 +14,14 @@ connection.connect(function (err) {
   console.log("connected as id " + connection.threadId);
 });
 
+
 app.use(express.json());
 
 
 app.get("/", (req, res) => {
   res.send("Welcome to my favorite movie list");
 });
+
 
 app.get("/api/movies", (req, res) => {
   connection.query("SELECT * from movies", (err, results) => {
@@ -30,9 +33,10 @@ app.get("/api/movies", (req, res) => {
   });
 });
 
+
 app.get("/api/movies/:id", (req, res) => {
   connection.query(
-    `SELECT * from movies WHERE id=?`,
+    `SELECT * from movies WHERE id=`,
     [req.params.id],
     (err, results) => {
       if (err) {
@@ -78,10 +82,49 @@ app.post("/api/movies", (req, res) => {
   );
 });
 
+
+app.put("/api/users/:id", (req, res) => {
+
+  const idUser = req.params.id;
+
+
+  const newUser = req.body;
+
+
+  connection.query(
+    "UPDATE users SET ? WHERE id = ?",
+    [newUser, idUser],
+    (err, results) => {
+      if (err) {
+        console.log(err);
+        res.status(500).send("Error updating a user");
+      } else {
+        res.status(200).send("User updated successfully 🎉");
+      }
+    }
+  );
+});
+
+
 app.get("/api/users", (req, res) => {
   res.status(401).send("Unauthorized");
 });
 
+app.post("/api/users", (req, res) => {
+  const { firstname, lastname, email } = req.body;
+  connection.query(
+    "INSERT INTO users(firstname, lastname, email) VALUES(?, ?, ?)",
+    [firstname, lastname, email],
+    (err, results) => {
+      if (err) {
+        console.log(err);
+        res.status(500).send("Error saving a User");
+      } else {
+        res.status(200).send("Successfully saved");
+      }
+    }
+  );
+});
 app.listen(port, () => {
   console.log(`Server is runing on 3000`);
 });
